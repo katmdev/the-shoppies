66a458227f43d4ea8026cb3494624588bb316563
katmdev <katherinedevrieze@gmail.com>
2020-09-08 03:37:12 -0400

    Nominations error handling

--- src/components/Nominations.js
+++ src/components/Nominations.js
@@ -7,24 +7,20 @@ const Nominations = ({nominations, setNominations}) => {
     };
     return (
         <section className="nominations">
-        <h2>Nominations</h2>
-        <ul>
-            {nominations.map(nomination => {
-                return (
-                    <li
-                        key={nomination.imdbID}
-                    >
-                        {nomination.Title} ({nomination.Year})
-                        <button 
-                            onClick={() => handleDelete(nomination)}
-                        >
-                            Remove
-                        </button>
-                    </li>
-                );
-            })}
-        </ul>
-    </section>
+            <h2>Nominations</h2>
+            <ul>
+                {nominations.map(nomination => {
+                    return (
+                        <li key={nomination.imdbID}>
+                            {nomination.Title} ({nomination.Year})
+                            <button onClick={() => handleDelete(nomination)}>
+                                Remove
+                            </button>
+                        </li>
+                    );
+                })};
+             </ul>
+        </section>
     )
 };
 
