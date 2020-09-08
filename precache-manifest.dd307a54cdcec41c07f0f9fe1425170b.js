44748546d084fc4d9be70669372a775fac60081a
katmdev <katherinedevrieze@gmail.com>
2020-09-08 03:04:53 -0400

    Updates

---
+++ precache-manifest.dd307a54cdcec41c07f0f9fe1425170b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76baa3469944a82cb3804e5726af84a",
+    "url": "/the-shoppies/index.html"
+  },
+  {
+    "revision": "60adb7b571bdba26f0e7",
+    "url": "/the-shoppies/static/css/main.c3954341.chunk.css"
+  },
+  {
+    "revision": "441741f86ab06092ce8d",
+    "url": "/the-shoppies/static/js/2.beddd9b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-shoppies/static/js/2.beddd9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60adb7b571bdba26f0e7",
+    "url": "/the-shoppies/static/js/main.c2258d6a.chunk.js"
+  },
+  {
+    "revision": "3028237cd38fe8ab6725",
+    "url": "/the-shoppies/static/js/runtime-main.7a5fa803.js"
+  }
+]);
